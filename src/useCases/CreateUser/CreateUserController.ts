57637fc12dcef3899c905c9a10b9b0a0d8be1f18
ArianNexux <ariannexux0101@gmail.com>
2022-01-09 20:03:36 +0100

    refactor: add middleware to handle exception

--- src/useCases/CreateUser/CreateUserController.ts
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -9,7 +9,7 @@ export class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, email, password } = request.body;
 
-    try {
+
       await this.createUserUseCase.execute({
         name,
         email,
@@ -17,10 +17,6 @@ export class CreateUserController {
       })
   
       return response.status(201).send();  
-    } catch (err) {
-      return response.status(400).json({
-        message: err.message || 'Unexpected error.'
-      })
-    }
+
   }
 }
