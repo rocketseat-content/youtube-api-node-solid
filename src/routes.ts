7f772c215980bcae1b56fa63043a5e18f70eab97
ArianNexux <ariannexux0101@gmail.com>
2022-01-09 19:59:05 +0100

    refactor: call handle method directly

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,6 @@ import { createUserController } from "./useCases/CreateUser";
 
 const router = Router()
 
-router.post('/users', (request, response) => {
-  return createUserController.handle(request, response);
-});
+router.post('/users',  createUserController.handle);
 
 export { router }
